04c91e83b9c64d156cc6409a30a09974421a3e6d
cristian_puig <cristian.puig@est.fib.upc.edu>
2017-05-14 19:40:48 +0200

    añadir info de proyecto y autor

--- plugin.php
+++ plugin.php
@@ -1,3 +1,10 @@
 <?php
 
+return array(
+    'id' =>             'ostocket:stock', 
+    'name' =>           'Stock controller in tickets',
+    'author' =>         'pgcristian',
+    'description' =>    'Possibility to link stocks to tickets',
+);
+
 ?>
