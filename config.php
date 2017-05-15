30664cbcb38c07a3fc483c1fd35e82845590c4cb
cristian_puig <cristian.puig@est.fib.upc.edu>
2017-05-15 23:36:52 +0200

    Empezando config

--- config.php
+++ config.php
@@ -1,1 +1,15 @@
 <?php
+
+
+class StockConfig extends PluginConfig{
+    function getOptions() {
+ 
+    }
+    
+    function pre_save(&$config, &$errors) {
+        global $msg;
+        if (!$errors)
+            $msg = 'OK';
+        return true;
+    }
+}
